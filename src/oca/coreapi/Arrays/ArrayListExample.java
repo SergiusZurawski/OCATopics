e31aed2b839aa216576ab08dc4c3cbf2332396d3
sergii zhuravskyi <szhuravskyi@navinet.net>
2017-02-01 14:34:50 +0000

    Arraylist

---
+++ src/oca/coreapi/Arrays/ArrayListExample.java
@@ -0,0 +1,147 @@
+package oca.coreapi.Arrays;
+
+import java.util.*; // import whole package including ArrayList
+import java.util.ArrayList; // import just ArrayList
+
+/** ArrayList is an ordered sequence that allows duplicates.
+    <E> - convention in generics to mean “any class that this array can hold.”
+    ArrayList(without <>) - means AraayList<Object>
+ */
+public class ArrayListExample {
+    //Before Java 5
+    ArrayList list1 = new ArrayList();
+    ArrayList list2 = new ArrayList(10);
+    ArrayList list3 = new ArrayList(list2);
+    //Java 5 Generics. <> - dimonds.
+    ArrayList<String> list4 = new ArrayList<String>();
+    // Java 7
+    ArrayList<String> list5 = new ArrayList<>();
+
+    public void initArrayList(){
+        ArrayList<String> places0 = new ArrayList<String>();
+        places0.add("Buenos Aires");
+        places0.add("Córdoba");
+        places0.add("La Plata");
+
+        ArrayList<String> places1 = new ArrayList<String>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata"));
+
+        /**Double brace initialization; creates anonius class, not efficient loading time, affects gc*/
+        ArrayList<String> list = new ArrayList<String>() {{
+            add("A");
+            add("B");
+            add("C");
+        }};
+
+        /**IMMUTABLE*/
+        /**utilities and List Not ArrayList*/
+        List<String> places2 = Arrays.asList("Buenos Aires", "Córdoba", "La Plata");
+        /**collection and List Not ArrayList*/
+        List<String> places3 = Collections.singletonList("Buenos Aires");
+        /**IMMUTABLE to MUTABLE conversion*/
+        ArrayList<String> places = new ArrayList<>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata"));
+    }
+
+    /**Methods*/
+    /**
+     boolean add(E element)     //return boolean is always true for ArrayList. It needs to be there due to collection Interface
+     void add(int index, E element)
+     */
+    public void addExample(){
+        ArrayList list = new ArrayList();
+        list.add("hawk");         // [hawk]
+        list.add(Boolean.TRUE);   // [hawk, true]
+        System.out.println(list); // [hawk, true]
+
+        //safe version
+        ArrayList<String> safer = new ArrayList<>();
+        safer.add("sparrow");
+        //safer.add(Boolean.TRUE); // DOES NOT COMPILE
+
+        List<String> birds = new ArrayList<>();
+        birds.add("hawk"); // [hawk]
+        birds.add(1, "robin"); // [hawk, robin]
+        birds.add(0, "blue jay"); // [blue jay, hawk, robin]
+        birds.add(1, "cardinal"); // [blue jay, cardinal, hawk, robin]
+        System.out.println(birds); // [blue jay, cardinal, hawk, robin]
+    }
+
+    /**
+     boolean remove(Object object)
+     E remove(int index)
+     */
+    public void removeExample(){
+        List<String> birds = new ArrayList<>();
+        birds.add("hawk"); // [hawk]
+        birds.add("hawk"); // [hawk, hawk]
+        System.out.println(birds.remove("cardinal")); // prints false
+        System.out.println(birds.remove("hawk")); // prints true
+        System.out.println(birds.remove(0)); // prints hawk
+        System.out.println(birds);
+    }
+
+    /** E set(int index, E newElement)
+        -  replace*/
+
+    public void setExample(){
+        List<String> birds = new ArrayList<>();
+        birds.add("hawk"); // [hawk]
+        System.out.println(birds.size()); // 1
+        birds.set(0, "robin"); // [robin]
+        System.out.println(birds.size()); // 1
+        birds.set(1, "robin"); // IndexOutOfBoundsException
+    }
+
+    /**
+     boolean isEmpty()
+     int size()
+    */
+
+    public void sizeExample(){
+        List<String> birds = new ArrayList<>();
+        System.out.println(birds.isEmpty()); // true
+        System.out.println(birds.size());    // 0
+        birds.add("hawk");                   // [hawk]
+        birds.add("hawk");                   // [hawk, hawk]
+        System.out.println(birds.isEmpty()); // false
+        System.out.println(birds.size());    // 2
+    }
+
+    /**  void clear()  */
+
+    public void clearExample() {
+        List<String> birds = new ArrayList<>();
+        birds.add("hawk");                   // [hawk]
+        birds.add("hawk");                   // [hawk, hawk]
+        System.out.println(birds.isEmpty()); // false
+        System.out.println(birds.size());    // 2
+        birds.clear();                       // []
+        System.out.println(birds.isEmpty()); // true
+        System.out.println(birds.size());    // 0
+    }
+
+    /**  boolean contains(Object object)
+        This method calls equals() on each element of the ArrayList to see whether there are
+        any matches. Since String implements equals(), this works out well.
+     */
+    public void containsExample() {
+        List<String> birds = new ArrayList<>();
+        birds.add("hawk"); // [hawk]
+        System.out.println(birds.contains("hawk"));  // true
+        System.out.println(birds.contains("robin")); // false
+    }
+
+    /**  boolean equals(Object object)  */
+
+    public void equalsExample() {
+        List<String> one = new ArrayList<>();
+        List<String> two = new ArrayList<>();
+        System.out.println(one.equals(two)); // true
+        one.add("a");                        // [a]
+        System.out.println(one.equals(two)); // false
+        two.add("a");                        // [a]
+        System.out.println(one.equals(two)); // true
+        one.add("b");                        // [a,b]
+        two.add(0, "b");                     // [b,a]
+        System.out.println(one.equals(two)); // false
+    }
+}
--- src/oca/coreapi/Arrays/ArraysExample.java
+++ src/oca/coreapi/Arrays/ArraysExample.java
@@ -67,4 +67,37 @@ public class ArraysExample {
         numbers[numbers.length] = 5;
         for (int i = 0; i <= numbers.length; i++) numbers[i] = i + 5;
     }
+
+    /**Multidimentional arryas*/
+    int[][] vars1; // 2D array
+    int vars2 [][]; // 2D array
+    int[] vars3[]; // 2D array
+    int[] vars4 [], space [][]; // a 2D AND a 3D array
+    int[][][][][] varsM; // 5D array
+    /**Initialization of MD arr*/
+    String [][] rectangle = new String[3][2];  //The result of this statement is an array rectangle
+    int[][] differentSize = {{1, 4}, {3}, {9,8,7}};
+
+    public void multiDimantionalArray(){
+        /**Init just array dimention */
+        int [][] args = new int[4][];
+        // Then init each arrays separately.
+        args[0] = new int[5];
+        args[1] = new int[3];
+    }
+
+    public void usingDimantionalArray() {
+        int[][] twoD = new int[3][2];
+        for (int i = 0; i < twoD.length; i++) {
+            for (int j = 0; j < twoD[i].length; j++)
+                System.out.print(twoD[i][j] + " "); // print element
+            System.out.println(); // time for a new row
+        }
+        // for each
+        for (int[] inner : twoD) {
+            for (int num : inner)
+                System.out.print(num + " ");
+            System.out.println();
+        }
+    }
 }
