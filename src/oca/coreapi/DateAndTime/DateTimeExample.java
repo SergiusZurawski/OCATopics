3ad00bb6957d02d938f02c11cb1747493b59141e
Sergi Zhuravski <sergi.zhuravski@gmail.com>
2017-02-01 19:16:38 +0000

    Date Time

--- src/oca/coreapi/DateAndTime/DateTimeExample.java
+++ src/oca/coreapi/DateAndTime/DateTimeExample.java
@@ -1,7 +1,19 @@
 package oca.coreapi.DateAndTime;
 
+import java.time.*; // import time classes
 /**
- * Created by Serg on 2/1/2017.
+    LocalDate -     Contains just a date—no time and no time zone.
+    LocalTime -     Contains just a time—no date and no time zone.
+    LocalDateTime - Contains both a date and time but no time zone
+    ZonedDateTime
+
+    Java uses T to separate the date and time when converting LocalDateTime to a String.
  */
 public class DateTimeExample {
+
+    public void timeEx(){
+        System.out.println(LocalDate.now());     //2015-01-20
+        System.out.println(LocalTime.now());     //12:45:18.401
+        System.out.println(LocalDateTime.now()); //2015-01-20T12:45:18.401
+    }
 }
