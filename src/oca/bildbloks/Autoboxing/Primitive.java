00bd1c936b9f3045135d0c1c5dcaaa33aed08373
SergiusZurawski <sergi.zhuravski@gmail.com>
2017-08-19 18:16:08 +0100

    Added page 171

--- src/oca/bildbloks/Autoboxing/Primitive.java
+++ src/oca/bildbloks/Autoboxing/Primitive.java
@@ -80,24 +80,17 @@ class OrderOfCall {
         //                     Autoboxed Integer "Integer'
         int var = 1;
         co.flyPriority(var);
-
     }
+
 }
 /** Var Args Example*/
 class Glider2 {
-    public static String glide(String s) {
-        return "1";
-    }
-    public static String glide(String... s) {
-        return "2";
-    }
-    public static String glide(Object o) {
-        return "3";
-    }
-    public static String glide(String s, String t) {
-        return "4";
-    }
-    public static void main(String[] args) {
+    public static String glide(String s)           {return "1";}
+    public static String glide(String... s)        {return "2";}
+    public static String glide(Object o)           {return "3";}
+    public static String glide(String s, String t) {return "4";}
+
+    public static void callExample() {
         System.out.print(glide("a"));
         System.out.print(glide("a", "b"));
         System.out.print(glide("a", "b", "c"));
